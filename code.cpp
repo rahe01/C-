aa45af4dd10bfefaafcfeaac5c53088200a6e7ca
rahe01 <rahatrahe01@gmail.com>
2026-07-21 23:05:00 +0600

    first

---
+++ code.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // cout << "Hello world" << endl;
+    // cout << "Hello world Rahe \n";
+    int a = 78;
+    char grade = 'A';
+    float PI = 3.14f;
+    double number = 122.33;
+
+    cout << typeid(PI).name();
+
+    // cout << "Rahat Ahmed \nRahe";
+
+    // cout << sizeof(a);
+
+    return 0;
+}
